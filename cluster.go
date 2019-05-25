028f6cc66ad9ee242b01149ca7afdd7cfaa416af
Fred de Villamil <fred@t37.net>
2019-05-25 09:31:47 +0200

    Adds cluster management file

---
+++ cluster.go
@@ -0,0 +1,18 @@
+package elasticsearch
+
+import (
+	"github.com/tidwall/gjson"
+)
+
+// Getsettings returns the cluster global settings
+func (c *Cluster) Getsettings() string {
+	return (c.buildgetquery("_settings"))
+}
+
+// Getsetting returns a defined setting
+func (c *Cluster) Getsetting(setting string) string {
+	settings := c.Getsettings()
+
+	result := gjson.Get(settings, setting)
+	return (result.String())
+}
