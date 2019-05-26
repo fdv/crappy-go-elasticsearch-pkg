71cf94ccd0d06720d62cf24eec26c8028569b5fe
Fred de Villamil <fred@t37.net>
2019-05-26 09:52:38 +0200

    Adds tests for cluster.Getsettings

--- elasticsearch_test.go
+++ elasticsearch_test.go
@@ -1,12 +1,28 @@
 package elasticsearch
 
 import (
+	"log"
+	"net"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 var cluster = Init("http://localhost", 9200)
+var clustersettings = `{
+	"persistent":{
+		"cluster": {
+			"routing": {
+				"allocation": {
+					"enable": "primary"
+				}
+			}
+		}
+	},
+	"transient":{}
+	}`
 
 func TestInit(t *testing.T) {
 	assert.Equal(t, cluster.URI, "http://localhost", "Cluster URI Should be http://localhost")
@@ -27,4 +43,45 @@ func TestGeturl(t *testing.T) {
 
 	assert.Equal(t, cluster.Geturl(), "http://localhost:9200", "Cluster URL should be http://localhost:9200")
 
+}
+
+func TestGetsettings(t *testing.T) {
+	resp := clustersettings
+	expected := `{
+	"persistent":{
+		"cluster": {
+			"routing": {
+				"allocation": {
+					"enable": "primary"
+				}
+			}
+		}
+	},
+	"transient":{}
+	}`
+
+	ts := mockescluster(resp)
+	defer ts.Close()
+
+	result := cluster.Getsettings()
+	assert.Equal(t, expected, result, "Error in cluster settings")
+}
+
+func mockescluster(resp string) *httptest.Server {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+
+		w.Write([]byte(resp))
+	}
+	l, err := net.Listen("tcp", "127.0.0.1:9200")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r)
+	}))
+	ts.Listener.Close()
+	ts.Listener = l
+	ts.Start()
+	return ts
 }
